02ed534a28f39211bd74377488f62a33a33e5b65
Jonathan Gabler <jgabler5025@gmail.com>
2022-06-02 20:05:04 -0400

    Edits to mdloader.bat, readme.md
    Removed non-compliant file from directory structure, revised instructions to be clearer, added file checks to custom firmware upload, and added support for the Drop Ctrl.

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # mdloaderscript
-A command line script to provide ease of use on Windows for Drop's (formerly Massdrop) QMK firmware loader.
-Currently only supports the Drop Shift.
+A command line shell to provide ease of use on Windows for Drop's (formerly Massdrop) QMK firmware loader.
+Currently supports the Drop Shift and Drop Ctrl.
 
 # Instructions:
 - Download a copy of the repository.
-- Download a copy of mdloader from the Massdrop repository.
-    - https://github.com/Massdrop/mdloader/releases
+- Download a copy of [mdloader](https://github.com/Massdrop/mdloader/releases) from the Massdrop repository.
 - Place the unzipped files and mdloader.exe in the same folder
-- Place any QMK bin files in the appropriate folder for their keyboard
+- Download default config files from the [Drop Configurator](https://drop.com/mechanical-keyboards/configurator) and place them in the defaults folder with the following file names:
+    - Drop Shift:
+        - shift_default.bin
+    - Drop Ctrl:
+        - Original: ctrl_default_original.bin
+        - Modern: ctrl_default_modern.bin
+- Place any Drop Configurator bin files in the appropriate folder for their keyboard
 - Run mdloader.bat to be given a menu style prompt
--- mdloader.bat
+++ mdloader.bat
@@ -1,19 +1,50 @@
 @echo off
 
+:mainMenu {
+
+    ::Print out the main menu
+    echo Main Menu -
+    echo 1. Drop Ctrl
+    echo 2. Drop Shift
+    set /p choice="Your choice (q to cancel): "
+    echo.
+
+    ::Go to appropriate part of code given correct choice
+    if %choice% == 1 (
+        goto ctrlMenu 
+    )
+    if %choice% == 2 (
+        goto shiftMenu
+    )
+    if %choice% == q (
+        goto scriptExit
+    )
+
+    ::Else loop menu
+    echo Invalid choice, please enter a valid option
+    echo.
+    goto mainMenu
+}
+
 :shiftMenu {
 
     ::Print out the menu 
     echo Drop Shift -
     echo 1. Reset Shift
     echo 2. Upload new firmware
-    echo 3. Exit script
-    set /p choice="Your choice: "
+    set /p choice="Your choice (q to cancel): "
     echo.
 
     ::Go to appropriate part of code if conditional is correct
-    if %choice% == 1 ( goto shiftReset )
-    if %choice% == 2 ( goto shiftFirmware )
-    if %choice% == 3 ( goto scriptExit)
+    if %choice% == 1 (
+        goto shiftReset
+    )
+    if %choice% == 2 (
+        goto shiftFirmware
+    )
+    if %choice% == q (
+        goto mainMenu
+    )
 
     ::Defacto else
     echo Invalid choice, please enter a valid option.
@@ -23,11 +54,18 @@
 
 :shiftReset {
 
+    ::Checks if reset firmware is in defaults folder
+    if not exist "defaults\shift_default.bin" (
+        echo Please restore the Shift default firmware to the defaults folder under the filename shift_default.bin.
+        echo.
+        goto shiftMenu
+    )
+
     ::Prints out DFU instructions
-    call :shiftInstructions
+    call :dfuInstructions
 
     ::Calls mdloader.exe with appropriate flags
-    mdloader.exe --first --download shift\shift_default.bin --restart
+    mdloader.exe --first --download defaults\shift_default.bin --restart
     echo.
 
     ::Returns to menu
@@ -42,11 +80,28 @@
     echo.
 
     ::Asks for file name
-    set /p file_id="Please enter the file name you are trying to flash: "
+    set /p file_id="Please enter the file name you are trying to flash (q to cancel, r to refresh file list): "
     echo.
 
+    ::Returns to menu if user cancels
+    if %file_id% == q (
+        goto shiftMenu
+    )
+
+    ::Loops back to refresh file list
+    if %file_id% == r (
+        goto shiftFirmware
+    )
+
+    ::Loops back if an invalid file name is entered
+    if not exist "shift\"%file_id% (
+        echo Invalid file name. Please try again.
+        echo.
+        goto shiftFirmware
+    )
+
     ::Prints out DFU instructions
-    call :shiftInstructions
+    call :dfuInstructions
     
     ::Calls mdloader.exe with user input file and appropriate flags
     mdloader.exe --first --download shift\%file_id% --restart
@@ -56,8 +111,137 @@
     goto shiftMenu
 }
 
-::Prints out Shift DFU instructions
-:shiftInstructions {
+:ctrlMenu {
+
+    ::Print out the menu 
+    echo Drop Ctrl -
+    echo 1. Reset Ctrl
+    echo 2. Upload new firmware
+    set /p choice="Your choice (q to cancel): "
+    echo.
+
+    ::Go to appropriate part of code if conditional is correct
+    if %choice% == 1 (
+        goto ctrlReset
+    )
+    if %choice% == 2 (
+        goto ctrlFirmware
+    )
+    if %choice% == q (
+        goto mainMenu
+    )
+
+    ::Defacto else
+    echo Invalid choice, please enter a valid option.
+    echo.
+    goto ctrlMenu
+}
+
+:ctrlReset {
+
+    ::Prints reset options
+    echo Drop Ctrl Reset Options:
+    echo 1. Original firmware
+    echo 2. Modern firmware
+    set /p choice="Your choice (q to cancel): "
+    echo.
+
+    ::Backs out of reset menu
+    if %choice% == q(
+        goto ctrlMenu
+    )
+
+    ::Original firmware reset
+    if %choice% == 1(
+
+        ::Checks if reset firmware is in defaults folder
+        if not exist "defaults\ctrl_default_original.bin" (
+            echo Please restore the Ctrl original default firmware to the defaults folder under the filename ctrl_default_original.bin.
+            echo.
+            goto ctrlMenu
+        )
+
+        ::Prints out DFU instructions
+        call :dfuInstructions
+
+        ::Calls mdloader.exe with appropriate flags
+        mdloader.exe --first --download defaults\ctrl_default_original.bin --restart
+        echo.
+
+        ::Returns to menu
+        goto ctrlMenu
+
+    )
+
+    ::Original firmware reset
+    if %choice% == 2(
+        
+        ::Checks if reset firmware is in defaults folder
+        if not exist "defaults\ctrl_default_modern.bin" (
+            echo Please restore the Ctrl modern default firmware to the defaults folder under the filename ctrl_default_modern.bin.
+            echo.
+            goto ctrlMenu
+        )
+
+        ::Prints out DFU instructions
+        call :dfuInstructions
+
+        ::Calls mdloader.exe with appropriate flags
+        mdloader.exe --first --download defaults\ctrl_default_modern --restart
+        echo.
+
+        ::Returns to menu
+        goto ctrlMenu
+
+    )
+
+    ::Defacto else, catches invalid inputs
+    echo Invalid choice, please enter a valid option.
+    echo.
+    goto ctrlMenu
+}
+
+:ctrlFirmware {
+
+    ::Prints out .bin files in keyboard folder
+    echo Available files:
+    dir ctrl\*.bin /b
+    echo.
+
+    ::Asks for file name
+    set /p file_id="Please enter the file name you are trying to flash (q to cancel, r to refresh file list): "
+    echo.
+
+    ::Returns to menu if user cancels
+    if %file_id% == q (
+        goto ctrlMenu
+    )
+
+    ::Loops back to refresh file list
+    if %file_id% == r (
+        goto ctrlFirmware
+    )
+
+    ::Loops back if an invalid file name is entered
+    if not exist "ctrl\"%file_id% (
+        echo Invalid file name. Please try again.
+        echo.
+        goto ctrlFirmware
+    )
+
+    ::Prints out DFU instructions
+    call :dfuInstructions
+    
+    ::Calls mdloader.exe with user input file and appropriate flags
+    mdloader.exe --first --download ctrl\%file_id% --restart
+    echo.
+
+    ::Returns to menu
+    goto ctrlMenu
+}
+
+::Prints out DFU instructions
+:dfuInstructions {
     echo Hold FN+B for three seconds...
     echo.
     goto :eof
